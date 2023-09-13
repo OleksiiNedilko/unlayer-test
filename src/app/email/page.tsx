286b53a689b3c9f26e4767ef5a6d0fd06156ab5f
MaverickATC <atc1991@rocketmail.com>
2023-09-13 23:20:57 +0300

    fix no-window error

--- src/app/email/page.tsx
+++ src/app/email/page.tsx
@@ -1,37 +1,9 @@
-'use client'
-
-import styles from './page.module.css'
-import EmailEditor, { EditorRef, EmailEditorProps } from 'react-email-editor';
-import {useRef} from 'react'
+import {Editor} from '@/components/editor'
 
 export default function Email() {
-  const emailEditorRef = useRef<EditorRef>(null);
-
-  const exportHtml = () => {
-    const unlayer = emailEditorRef.current?.editor;
-
-    unlayer?.exportHtml((data) => {
-      const { design, html } = data;
-      console.log(html);
-      console.log('--------------------')
-      console.log(design)
-    });
-  };
-
-  const onReady: EmailEditorProps['onReady'] = (unlayer) => {
-    // editor is ready
-    // you can load your template here;
-    // the design json can be obtained by calling
-    // unlayer.loadDesign(callback) or unlayer.exportHtml(callback)
-
-    // const templateJson = { DESIGN JSON GOES HERE };
-    // unlayer.loadDesign(templateJson);
-  };
-
   return (
     <main >
-      <button title="see result in console" onClick={exportHtml}>Export</button>
-      <EmailEditor ref={emailEditorRef} onReady={onReady} options={{displayMode:"email"}} minHeight={700}  />
+      <Editor type="email"/>
     </main>
   )
 }
---
+++ src/components/editor.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import {FC, useRef} from 'react'
+import EmailEditor, {EditorRef, EmailEditorProps} from 'react-email-editor'
+
+export const Editor:FC<{type: 'web'|'email'}> = ({type}) => {
+  const emailEditorRef = useRef<EditorRef>(null);
+
+  const exportHtml = () => {
+    const unlayer = emailEditorRef.current?.editor;
+
+    unlayer?.exportHtml((data) => {
+      const { design, html } = data;
+      console.log(html);
+      console.log('--------------------')
+      console.log(design)
+    });
+  };
+
+  const onReady: EmailEditorProps['onReady'] = (unlayer) => {
+    // editor is ready
+    // you can load your template here;
+    // the design json can be obtained by calling
+    // unlayer.loadDesign(callback) or unlayer.exportHtml(callback)
+
+    // const templateJson = { DESIGN JSON GOES HERE };
+    // unlayer.loadDesign(templateJson);
+  };
+
+  return (<><button title="see result in console" onClick={exportHtml}>Export</button>
+    <EmailEditor ref={emailEditorRef} onReady={onReady} options={{displayMode:type}} minHeight={700}  /></>)
+}
